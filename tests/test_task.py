fa638948d63896da14580cfe4a79653777c1b99d
benhid <antoniobenitezhid@gmail.com>
2023-10-24 09:00:00 +0200

    fix: remove old test

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,5 +1,5 @@
-from dramax.models.task import Task
 import pytest
+from dramax.models.task import Task
 from pydantic import ValidationError
 
 
@@ -18,11 +18,6 @@ def test_task_name_without_spaces_and_dots():
         Task(name="test", image="test_image.png")
     except ValidationError:
         pytest.fail("ValidationError was not expected")
-
-
-def test_task_inputs_without_slash():
-    with pytest.raises(ValidationError):
-        Task(name="test", image="busybox", inputs=[{"path": "taskName.rest.of.path"}])
 
 
 def test_task_inputs_with_slash():
