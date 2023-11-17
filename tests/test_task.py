ab8e20493c38b3dd02a829562670842bd4d779d9
benhid <antoniobenitezhid@gmail.com>
2023-11-17 10:33:16 +0100

    fix: add tasks ids

--- tests/test_task.py
+++ tests/test_task.py
@@ -5,23 +5,23 @@ from pydantic import ValidationError
 
 def test_task_name_with_spaces():
     with pytest.raises(ValidationError):
-        Task(name="test task", image="busybox")
+        Task(id="test", name="test task", image="busybox")
 
 
 def test_task_name_with_dots():
     with pytest.raises(ValidationError):
-        Task(name="test.task", image="busybox")
+        Task(id="test", name="test.task", image="busybox")
 
 
 def test_task_name_without_spaces_and_dots():
     try:
-        Task(name="test", image="test_image.png")
+        Task(id="test", name="test", image="test_image.png")
     except ValidationError:
         pytest.fail("ValidationError was not expected")
 
 
 def test_task_inputs_with_slash():
     try:
-        Task(name="test", image="busybox", inputs=[{"path": "taskName/rest/of/path"}])
+        Task(id="test", name="test", image="busybox", inputs=[{"path": "taskName/rest/of/path"}])
     except ValidationError:
         pytest.fail("ValidationError was not expected")
