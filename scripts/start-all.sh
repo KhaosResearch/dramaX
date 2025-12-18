dac5b05f48a77d23569f066bc55a580ff3480a1c
joseacuesta <061972018x@uma.es>
2025-12-18 14:16:55 +0100

    chore(sh): run server and worker instead of worker and server

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-echo "--- Primer Ciclo: Inicialización y Parada Forzada ---"
+exec dramax server &
 
-nohup dramax worker --processes 1 &
-
-sleep 10
-
-echo "Terminando procesos dramax con killall..."
-# TODO: Solve error, worker not working the first time
-killall dramax
-
-nohup dramax worker --processes 1 &
-
-exec dramax server
+nohup dramax worker --processes 1
